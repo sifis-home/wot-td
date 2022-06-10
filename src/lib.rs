f8802123f9006d4051acf59c1c1afbdefb629b37
Luca Barbato <lu_zero@gentoo.org>
2022-06-10 09:38:28 +0200

    docs: Document the crate
    First pass.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
+//! Web of Things Thing Description manipulation
+//!
+//! Create, parse and modify [Thing Descriptions](https://www.w3.org/TR/wot-thing-description/):
+//! > A Thing Description describes the metadata and interfaces of Things,
+//! > where a Thing is an abstraction of a physical or virtual entity that
+//! > provides interactions to and participates in the Web of Things.
+//!
+//! The crate relies on [serde](https://docs.rs/serde) to serialize and deserialize and provides
+//! an high level builder to ensure a valid TD is built and a set of accessors to make easier to
+//! operate with complex descriptions.
+
 pub mod builder;
 pub mod thing;
