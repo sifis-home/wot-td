a085b4121aafd872498ab832efae75b8780d1f5f
Luca Barbato <lu_zero@gentoo.org>
2022-06-19 11:12:48 +0200

    docs(builder): Document human_readable_info

--- src/builder/human_readable_info.rs
+++ src/builder/human_readable_info.rs
@@ -1,23 +1,51 @@
+//! Human readable information and semantic tagging
+//!
+//! This module contains the logic shared across multiple builders for the respective
+//! Thing Description Vocabulary definitions.
+
 use crate::thing::MultiLanguage;
 
 use super::MultiLanguageBuilder;
 
+/// Human readable informations and semantic tagging
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct HumanReadableInfo {
+    /// JSON-LD @type
+    /// The meaning of each @type is given by the current JSON-LD @context.
     pub(super) attype: Option<Vec<String>>,
+    /// Human readable title in the default language
     pub(super) title: Option<String>,
+    /// Human redable title, multilanguage
     pub(super) titles: Option<MultiLanguage>,
+    /// Human readable description in the default language
     pub(super) description: Option<String>,
+    /// Human readable description, multilanguage
     pub(super) descriptions: Option<MultiLanguage>,
 }
 
+/// Trait shared across builders dealing with the same information
 pub trait BuildableHumanReadableInfo {
+    /// Set JSON-LD @type
+    ///
+    /// It can be called as many times as needed to add multiple @types.
     fn attype(self, value: impl Into<String>) -> Self;
+    /// Set the title
+    ///
+    /// Calling it multiple times overwrites the field.
     fn title(self, value: impl Into<String>) -> Self;
+    /// Set the translations of the title
+    ///
+    /// Calling it multiple times overwrites the field.
     fn titles<F>(self, f: F) -> Self
     where
         F: FnOnce(&mut MultiLanguageBuilder<String>) -> &mut MultiLanguageBuilder<String>;
+    /// Set the description
+    ///
+    /// Calling it multiple times overwrites the field.
     fn description(self, value: impl Into<String>) -> Self;
+    /// Set the translations of the description
+    ///
+    /// Calling it multiple times overwrites the field.
     fn descriptions<F>(self, f: F) -> Self
     where
         F: FnOnce(&mut MultiLanguageBuilder<String>) -> &mut MultiLanguageBuilder<String>;
