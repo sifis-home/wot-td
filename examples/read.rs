f9e96bb2fdb2377429cc827fc4ec013c6a64a7f2
Luca Barbato <lu_zero@gentoo.org>
2022-06-22 14:08:01 +0200

    feat: Add a tiny example
    Useful also to test thing descriptions produced by other
    implementations.

---
+++ examples/read.rs
@@ -0,0 +1,13 @@
+use wot_td::thing::Thing;
+
+fn main() {
+    let input = std::env::args()
+        .next()
+        .expect("It needs a path to a thing description file");
+
+    let thing: Thing =
+        serde_json::from_reader(std::fs::File::open(input).expect("File not accessible"))
+            .expect("Cannot parse the thing");
+
+    println!("{thing:?}");
+}
